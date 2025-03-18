e8bf0296880b4df616dafd716be3a958eb4d0031
shayd <shaydegani@yahoo.com>
2025-03-18 14:53:57 +0200

    init

---
+++ src/main/java/com/atnt/assignment/movie_ticket_booking_api/MovieTicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.atnt.assignment.movie_ticket_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingApiApplication.class, args);
+	}
+
+}
